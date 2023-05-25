c9d572c644e588594fe58c9be9e9d5c54dda2895
gk <gk@adamant.local>
2023-05-25 21:22:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# result = 0
+# count = 0
+# while True:
+#     number = int(input('Введите число\n'))
+#     if number >= 0:
+#         result += number
+#         count += 1
+#     if number < 0:
+#         break
+#     print('Сумма чисел = ', result)
+# print('Результат =', result / count)
+
+# result = 0
+# while True:
+#     number = int(input('Введите число\n'))
+#     if number == 0:                      #and number % 2 == 1 or number < 0 and number % 2 == 1: 0 четный
+#         break
+#     if number % 2 == 0:
+#         continue
+#     result += number
+# print('Сумма нечетных чисел = ', result)
+
+6.4 операторы использовать
+6.6 б + разность между ними
+6.42 а
